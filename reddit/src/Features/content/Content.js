3815def29492dfbad4e36e44c9a9f708f39d2434
Rachel Kaufman <16939591+rkaufman13@users.noreply.github.com>
2021-09-01 08:15:21 -0400

    add styling to better match figma

--- reddit/src/Features/content/Content.js
+++ reddit/src/Features/content/Content.js
@@ -1,15 +1,14 @@
+//todo: make each post its own Component
 function Content() {
     return (
      <div>
          <div className="post">
-             <h2>Post title</h2>
-         <img src="http://via.placeholder.com/800x500" alt="placeholder"/><br/>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+         <img src="http://via.placeholder.com/500x300" alt="placeholder"/><br/>
+         <h2>Post title</h2>
 </div>
-<div className="post">
-    <h2>Post title</h2>
-         <img src="http://via.placeholder.com/800x500" alt="placeholder"/><br/>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+<div className="post">    
+         <img src="http://via.placeholder.com/500x300" alt="placeholder"/><br/>
+         <h2>Post title</h2>
 </div>
      </div>
     );
